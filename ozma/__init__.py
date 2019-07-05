95d247672bdd5684338a622a57f784850a25e1a9
Landon Wark <lando.wark@gmail.com>
2019-07-05 09:02:52 -0500

    Rearranged code to prevent removal of ":" in rsync target.

--- ozma/__init__.py
+++ ozma/__init__.py
@@ -12,6 +12,10 @@ from .tools import transmission
 
 logger = logging.getLogger("ozma.parser")
 
+def rreplace(s, old, new, occurrence):
+     li = s.rsplit(old, occurrence)
+     return new.join(li)
+
 
 class MediaObject():
     def __init__(self, source_file, destination_dir, destination_file, rsync_user, rsync_pass):
@@ -59,8 +63,8 @@ class MediaManager():
             func()
             rsync_mkdirs = os.path.join(self.settings['make_dir_schema'].format(media_type=mediatype),
                                              os.path.split(self.final_filename)[0])
+            self.final_filename = self.final_filename.replace(":", "-")
             rsync_target = self.settings['rsync_schema'].format(media_type=mediatype) + self.final_filename
-            rsync_target = rsync_target.replace(":", "-")
             self.mediaobjs.append(MediaObject(filepath, rsync_mkdirs, rsync_target, self.settings['rsync_user'], self.settings['rsync_pass']))
 
 

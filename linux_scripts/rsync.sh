fa446e8b6b54693163d98138b1b0191a7ada6c45
Landon Wark <lando.wark@gmail.com>
2019-07-05 09:18:09 -0500

    Debugging for rsync.

--- linux_scripts/rsync.sh
+++ linux_scripts/rsync.sh
@@ -6,4 +6,8 @@ DESTINATION_FILE=$3
 USERNAME=$4
 PASSWORD=$5
 
+echo $SOURCE_FILE
+echo $DESTINATION_DIR
+echo $DESTINATION_FILE
+
 rsync -avzI --ignore-existing -e ssh --log-file=rsync.log --rsync-path="mkdir -p ${DESTINATION_DIR} && rsync"  --rsh="/usr/bin/sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no -l ${USERNAME}" "${SOURCE_FILE}" "${DESTINATION_FILE}"

a10888a7b361e25e380007125622da4774912a2c
Landon Wark <lando.wark@gmail.com>
2019-07-05 13:24:33 -0500

    Added timeout to rsync.

--- linux_scripts/rsync.sh
+++ linux_scripts/rsync.sh
@@ -10,4 +10,4 @@ echo $SOURCE_FILE
 echo $DESTINATION_DIR
 echo $DESTINATION_FILE
 
-rsync -avI --block-size=131072 -P --ignore-existing -e ssh --log-file=rsync.log --rsync-path="mkdir -p ${DESTINATION_DIR} && rsync"  --rsh="/usr/bin/sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no -l ${USERNAME}" "${SOURCE_FILE}" "${DESTINATION_FILE}"
+rsync -avI --timeout=60 --block-size=131072 -P --ignore-existing -e ssh --log-file=rsync.log --rsync-path="mkdir -p ${DESTINATION_DIR} && rsync"  --rsh="/usr/bin/sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no -l ${USERNAME}" "${SOURCE_FILE}" "${DESTINATION_FILE}"

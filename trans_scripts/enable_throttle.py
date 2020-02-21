52b5532eaa9b28c05d2dea0f60be598f8ee03fcf
Landon Wark <lando.wark@gmail.com>
2020-02-21 14:39:22 -0600

    Scripts to enable/disable throttling.

---
+++ trans_scripts/enable_throttle.py
@@ -0,0 +1,20 @@
+#!/home/landon/Scripts/ozmarenamer/venv/bin/python
+
+from transmission_rpc import Client, DEFAULT_PORT
+from ..ozma.setup import get_config
+import logging
+import os
+
+configs = get_config()
+logger = logging.getLogger("ozma.enable_throttle")
+try:
+    c = Client(port=DEFAULT_PORT, user=configs['transmission_user'], password=configs['transmission_pass'])
+except Exception as e:
+    logger.error(f"Error in getting transmission client: {e}")
+try:
+    completed_dir = configs['watch_dir']
+except Exception as e:
+    logger.error(f"Error in finding completed dir in settings: {e}")
+
+logger.info(f"Enabling throttling...")
+c.session.alt_speed_enabled = True

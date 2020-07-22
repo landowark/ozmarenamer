095f7aa0dc9bc33824d40a14dcca08e20dd5e3e2
Landon Wark <lando.wark@gmail.com>
2020-07-22 18:22:50 -0500

    Debugging smb protocol.

--- ozma/__init__.py
+++ ozma/__init__.py
@@ -269,7 +269,7 @@ def main(*args):
                     path_list = file.destination_file.split("/")
                     server_address = path_list[2]
                     share = path_list[3]
-                    file_path = "/".join(path_list[4:])
+                    file_path = "/".join(path_list[4:]).replace("\\", "\\\\")
                     logger.debug(f"File path {file_path}")
                     conn = SMBConnection(config['smb_user'], config['smb_pass'], "client", "host", use_ntlm_v2=True)
                     try:

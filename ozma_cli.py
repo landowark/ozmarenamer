daded3db1f2dfc30fdd5c10736cd6dfadcec7e90
Landon Wark <lando.wark@gmail.com>
2021-10-24 15:03:29 -0500

    Improving logging.

--- ozma_cli.py
+++ ozma_cli.py
@@ -3,10 +3,11 @@
 import ozma
 from ozma.setup import get_cliarg
 import logging
+import getpass
 
 logger = logging.getLogger("ozma.cli")
 
-logger.debug("Command line called.")
+logger.debug(f"Command line called by {getpass.getuser()}.")
 params = get_cliarg()
 logger.debug(f"Command line parameters: {params}")
 ozma.main(params)

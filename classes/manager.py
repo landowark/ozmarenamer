2147ed287f6771c9252e7f6e010b2973aa5f476b
Landon Wark <lando.wark@gmail.com>
2023-02-04 16:21:40 -0600

    Debugging manager.py adding single file.

--- classes/manager.py
+++ classes/manager.py
@@ -2,6 +2,7 @@ import os
 import logging
 from tools import extract_files_if_folder
 from classes.media import MediaObject
+from pathlib import Path
 
 logger = logging.getLogger(f"ozma.{__name__}")
 
@@ -16,18 +17,22 @@ class MediaManager(object):
         self.load_files()
 
 
-    def load_files(self):
-        for filepath in self.filepaths:
-            if filepath.is_dir():
-                logger.debug(f"Filepath {filepath} is a directory, extracting files.")
-                file_list = extract_files_if_folder(dir_path=filepath)
-                if file_list != []:
-                    logger.debug("Look out, it's recursion time!")
-                    # todo I've a bug. The first run through uses the specified config file, but the recursion falls back to
-                    for file in file_list:
-                        self.load_files(filepath=file)
+    def load_files(self, file:Path|None=None):
+        if file is None:
+            for filepath in self.filepaths:
+                if filepath.is_dir():
+                    logger.debug(f"Filepath {filepath} is a directory, extracting files.")
+                    file_list = extract_files_if_folder(dir_path=filepath)
+                    if file_list != []:
+                        logger.debug("Look out, it's recursion time!")
+                        # todo I've a bug. The first run through uses the specified config file, but the recursion falls back to
+                        for file in file_list:
+                            self.load_files(file=file)
+                    else:
+                        logger.error(f"There are no appropriate files in {filepath}")
                 else:
-                    logger.error(f"There are no appropriate files in {filepath}")
-            else:
-                new_medObj = MediaObject(filepath=filepath, **self.__dict__)
-                self.mediaobjs.append(new_medObj)
+                    new_medObj = MediaObject(filepath=filepath, **self.__dict__)
+                    self.mediaobjs.append(new_medObj)
+        else:
+            new_medObj = MediaObject(filepath=file, **self.__dict__)
+            self.mediaobjs.append(new_medObj)
--- readme.md
+++ readme.md
@@ -4,8 +4,9 @@
 The settings file should be placed in the root directory (ie ozma_renamer) and should look like this:
 
 ```yaml
-[settings]
-tv_schema = /home/user/tv/{series_name}/{series_name} Season{season_number}/{series_name}.S{season_number}E{episode_number}.{episode_name}.{extension}
-movie_schema = /home/user/movies/{movie_name} ({year_of_release})/{movie_name} ({year_of_release}).{extension}
-main_language = en
+tv:
+    tv_schema: /home/user/tv/{{series_name}}/{{series_name}} Season{{season_number}}/{{series_name}}.S{{season_number}}E{{episode_number}}.{{episode_name}}.{{extension}}
+movie:
+    movie_schema: /home/user/movies/{{movie_name}} ({{year_of_release}})/{{movie_name}} ({{year_of_release}}).{{extension}}
+main_language: en
 ```
